ca4315e53e77c0df4c57e3503b4f209e2b81ef49
deniskirbaba <kirbabadenis@gmail.com>
2024-12-26 15:39:06 +0300

    feat: add rate limiter

--- backend/app/legal_agent/chain.py
+++ backend/app/legal_agent/chain.py
@@ -9,6 +9,7 @@ from langchain_community.retrievers import BM25Retriever
 from langchain_core.prompts import (
     PromptTemplate,
 )
+from langchain_core.rate_limiters import InMemoryRateLimiter
 from langchain_core.runnables import RunnableLambda, RunnableParallel
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_mistralai.chat_models import ChatMistralAI
@@ -18,7 +19,27 @@ from nltk.tokenize import word_tokenize
 from ..config import settings
 from .structured_output import Rephrases
 
-# STEP 0
+# STEP 0: Initialization
+
+# About RPS with Mistral:
+# On their cite RPS = 1 sec, but in reality the rate limit error
+# can occurs even when time between requests about 3-4 sec, so it's not exact 1 sec.
+# Langchain has rate limiter, f.e. `InMemoryRateLimiter`, which can retries requests
+# when error occurred. BUT don't work with Runnables which called with .stream() or .astream()
+# In this chain we have 2 or 3 model invokations (based on path).
+# I made it so that streaming model call will be only at the final generation -
+# and there I added a custom function to allocate a certain time between API requests.
+# And all other model calls I made not via stream (but manually .invoke), so that the built-in rate limiter would work in them
+
+WAIT_BW_API_REQ = 3  # Wait time between last API call and final stremed API call
+RPS = 1  # `Real` RPS on Mistral cite (used in langchain RateLimiter)
+
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=(RPS - 0.25),
+    check_every_n_seconds=0.1,  # Wake up every 100 ms to check whether allowed to make a request,
+    max_bucket_size=10,  # Controls the maximum burst size.
+)
+
 # Logging configuration
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -30,19 +51,26 @@ mem = {
 }
 
 
+def save_api_invokation_time(x):
+    """
+    Just saves Mistral API request time into mem.
+    """
+    mem["last_mistral_api_req"] = time()
+    return x
+
+
 @traceable
 def check_rps(x):
     """
     Monitors time between Mistral API calls, in order to not exceed RPS.
     """
-    rps_limit = 2  # sec
     logger.info("Checking Mistral RPS violations...")
 
     time_since_last_call = time() - mem.get("last_mistral_api_req", 0)
     logger.info("Time since last Mistral API request: %.2f seconds", time_since_last_call)
 
-    if time_since_last_call < rps_limit:
-        sleep_duration = rps_limit - time_since_last_call
+    if time_since_last_call < WAIT_BW_API_REQ:
+        sleep_duration = WAIT_BW_API_REQ - time_since_last_call
         logger.info("RPS limit exceeded, sleeping for %.2f seconds", sleep_duration)
         sleep(sleep_duration)
 
@@ -63,6 +91,8 @@ cls_llm = ChatMistralAI(
     temperature=0.6,
     streaming=False,
     max_tokens=5,
+).with_retry(
+    retry_if_exception_type=(Exception,), wait_exponential_jitter=True, stop_after_attempt=5
 )
 
 # Load system prompt for classification
@@ -78,7 +108,12 @@ def prepare_input(query: str):
     return {"input": query}
 
 
-cls_chain = prepare_input | cls_prompt | check_rps | cls_llm
+cls_chain = (
+    prepare_input
+    | cls_prompt
+    | RunnableLambda(lambda x: cls_llm.invoke(x))
+    | save_api_invokation_time
+)
 
 # STEP 2-0: Chat response on non-legal assistance request
 
@@ -94,17 +129,27 @@ chat_llm = ChatMistralAI(
 with open("/app/app/legal_agent/prompts/chat_prompt.txt", "r") as f:
     chat_prompt = f.read()
 
-non_legal_chat_chain = RunnableLambda(lambda x: chat_prompt + mem["query"]) | check_rps | chat_llm
+non_legal_chat_chain = (
+    RunnableLambda(lambda x: chat_prompt + mem["query"])
+    | check_rps
+    | chat_llm
+)
 
 # STEP 2-1-0: Creating queries for retrieval
 
 # Queries generation model
-queries_gen_llm = ChatMistralAI(
-    api_key=settings.MISTRAL_API_KEY,
-    model_name="mistral-large-latest",
-    temperature=0.8,
-    streaming=False,
-).with_structured_output(Rephrases)
+queries_gen_llm = (
+    ChatMistralAI(
+        api_key=settings.MISTRAL_API_KEY,
+        model_name="mistral-large-latest",
+        temperature=0.8,
+        streaming=False,
+    )
+    .with_structured_output(Rephrases)
+    .with_retry(
+        retry_if_exception_type=(Exception,), wait_exponential_jitter=True, stop_after_attempt=5
+    )
+)
 
 # Load system prompt
 with open("/app/app/legal_agent/prompts/queries_gen_prompt.txt", "r") as f:
@@ -134,8 +179,8 @@ def queries_validation(rephrases: Rephrases | None) -> list[str]:
 
 queries_gen_chain = (
     RunnableLambda(lambda x: queries_gen_prompt.invoke({"input": mem["query"]}))
-    | check_rps
-    | queries_gen_llm
+    | RunnableLambda(lambda x: queries_gen_llm.invoke(x))
+    | save_api_invokation_time
     | queries_validation
 )
 
@@ -261,13 +306,12 @@ retrieval_chain = (
 
 # STEP 2-1-2: Legal assistance generation
 
-# Queries generation model
+# Legal assistance generation model
 legal_gen_llm = ChatMistralAI(
     api_key=settings.MISTRAL_API_KEY,
     model_name="mistral-large-latest",
     temperature=0.7,
-    streaming=True,
-)
+    streaming=True)
 
 # Load system prompt
 with open("/app/app/legal_agent/prompts/legal_gen_prompt.txt", "r") as f:
